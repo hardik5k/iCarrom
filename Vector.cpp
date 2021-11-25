736c4fe70e653220f0527e02d72f44f24109b97b
Hardik Khandelwal <77318140+hardik5k@users.noreply.github.com>
2021-11-25 09:57:36 +0530

    vector class

---
+++ Vector.cpp
@@ -0,0 +1,53 @@
+#include <cmath>
+
+class Vector{
+    private:
+        float x, y;
+
+    public:
+        Vector(float x, float y){
+            this->x = x;
+            this->y = y;
+        }
+
+        float getX(){
+            return this->x;
+        }
+        float getY(){
+            return this->y;
+        }
+
+        float getMagnitute(int square = 0){
+            return square ? pow(x,2) + pow(y,2) : sqrt(pow(x,2) + pow(y,2));
+        }
+        float getArgument(){
+            return atan(this->y/this->x); 
+        }
+        
+        float distanceFrom(Vector v){
+            return sqrt(pow((this->x - v.x), 2) + pow((this->y - v.y), 2));
+        }
+
+        float dotProduct(Vector v){
+            return (this->x * v.x + this->y * v.y);
+        }
+        
+        Vector multiply(float i){
+            Vector u(this->x * i, this->y * i);
+            return u;
+        }
+
+        Vector normalize(){
+            return this->multiply(1/this->getMagnitute());
+        }
+
+        Vector sub(Vector v){
+            Vector u(this->x - v.x, this->y - v.y);
+            return u;
+        }
+        Vector sub(Vector v){
+            Vector u(this->x + v.x, this->y + v.y);
+            return u;
+        }
+
+};
