6c5471809d13423a57c22636590e808fd654a538
harsh788 <Harsh.Shah@iiitb.org>
2021-12-05 17:16:48 +0530

    New commit(Added carrom board)

--- Board.cpp
+++ Board.cpp
@@ -1,12 +1,14 @@
+#include <SDL2/SDL.h>
+#include <SDL/SDL_image.h>
+
+
 class Board{
     private:
-        vector<> sides;
-        vector<> holes;
+        //vector<> sides;
+        //vector<> holes;
         int center_x, center_y;
-        int width, height; 
+        int width = 800, height = 600; 
         
         friend class EventHandler; 
-
-        
 
 };
